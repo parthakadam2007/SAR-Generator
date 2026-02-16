e720c29b9a17112cfd254369940a9c5339a531fe
Partha Kadam <parthakadam2007@gmail.com>
2026-02-17 02:18:30 +0530

    auth : main_service auth implemented

--- backend/main_service/src/main/java/com/SAR_Generator/main_service/services/user/UserSerivce.java
+++ backend/main_service/src/main/java/com/SAR_Generator/main_service/services/user/UserSerivce.java
@@ -1,5 +1,13 @@
 package com.SAR_Generator.main_service.services.user;
 
-public class UserSerivce {
-    public 
+import java.util.List;
+
+import com.SAR_Generator.main_service.dto.UserDTO;
+
+public interface UserSerivce {
+    public List<UserDTO> getAllUsers();
+    public UserDTO saveUser(UserDTO UserDTO);
+    public UserDTO getUserById(Long id);
+    public UserDTO getUserByEmail(String email);
+    public UserDTO updateUser(Long id, UserDTO UserDTO);
 }
